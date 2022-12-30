8163369ae27fc0bb42813f82f423e75a2894ad82
mithindev <mithindev1@gmail.com>
2022-12-30 08:58:22 +0530

    updated

--- C-LANGUAGE/MIDSEM-PRACTICE/ASSIGNMENT-3/Q24.c
+++ C-LANGUAGE/MIDSEM-PRACTICE/ASSIGNMENT-3/Q24.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <math.h>
+
+
+int length(int num);
 
 void main() {
+  printf("Enter the number :");
+  int num;
+  scanf("%d", &num);
+  int temp = num;
+  // int temp = num;
+  // int h = 0;
+  // for (int i = 0; i < length(num)/2;i++) {
+  //   int n1 = num/pow(10,length(num)-i-1);
+  //   int n2 = temp%10;
+  //   temp = temp/10;
+  //   if (n1 != n2) {
+  //     printf("False!");
+  //     h = h+1;
+  //   }
+
+  // }
+  // if(h == 0) {
+  //   printf("Palindrome");
+  // }
+
+  int num2 = 0;
+  for(int i = 0;i < length(num); i++) {
+    int n = temp%10;
+    num2 += n*pow(10,length(num)-i-1);
+    temp /=10;
+  }
+  printf("%d", num2);
+
+  if (num = num2) {
+    printf("Palindrome!");
+  } else {
+    printf("Not palindrome!");
+  }
 
 }
 
