d36acf7ceda29a88a600be9dcde53e3a29aa84cd
mithindev <mithindev1@gmail.com>
2023-02-02 07:37:09 +0530

    updated

---
+++ C-LANGUAGE/STRINGS/StringConcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+  char str1[20] = "Hello ";
+char str2[] = "World!";
+
+// Concatenate str2 to str1 (result is stored in str1)
+strcat(str1, str2);
+
+// Print str1
+printf("%s", str1);
+}
